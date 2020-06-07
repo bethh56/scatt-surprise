d2f1db633aa10620f0155c45d86a9a61732fc730
Beth <bethhnielsen56@gmail.com>
2020-06-07 11:29:08 -0500

    wip

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -3,10 +3,17 @@ import React from 'react';
 import './Home.scss';
 
 class Home extends React.Component {
+  editEvent = (e) => {
+    e.preventDefault();
+    const scatId = 'scat123456';
+    this.props.history.push(`/edit/${scatId}`);
+  }
+
   render() {
     return (
       <div className="Home">
         <h1>Home</h1>
+        <button className="btn btn-dark" onClick={this.editEvent}>Edit a Thing</button>
       </div>
     );
   }
