3b674e444181fac59b4a83d93a0c57bccea029ca
Beth <bethhnielsen56@gmail.com>
2020-06-07 18:58:19 -0500

    added data

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -5,19 +5,22 @@ import { Link } from 'react-router-dom';
 import './Home.scss';
 
 class Home extends React.Component {
-  editEvent = (e) => {
-    e.preventDefault();
-    const scatId = 'scat123456';
-    this.props.history.push(`/edit/${scatId}`);
+  state = {
+    scats: []
   }
 
   render() {
+    const { scats } = this.state;
+    const buildScatCards = scats.map((scat) => (
+      <ScatCard scat={scat}/>
+    ));
+
     return (
       <div className="Home">
         <h1>Home</h1>
-        <button className="btn btn-dark" onClick={this.editEvent}>Edit a Thing</button>
-        <Link to='/scats/s56465465'> View Single </Link>
-        <Link to='/new'> View New </Link>
+        <div className="d-flex flex-wrap">
+          {buildScatCards}
+        </div>
       </div>
     );
   }
---
+++ src/components/shared/ScatCard/ScatCard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './ScatCard.scss';
+
+class ScatCard extends React.Component {
+  render() {
+    const { scat } = this.props;
+    return (
+      <div className="ScatCard">
+        {scat.location}
+      </div>
+    )
+  }
+}
