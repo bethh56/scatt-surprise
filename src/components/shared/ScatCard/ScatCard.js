629f3faa4df42d2fe0fa77d9ee2529a66d70ce95
Beth <bethhnielsen56@gmail.com>
2020-06-09 18:17:32 -0500

    able to delete from card

--- src/components/shared/ScatCard/ScatCard.js
+++ src/components/shared/ScatCard/ScatCard.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import PropTypes from 'prop-types';
 import './ScatCard.scss';
 import scatShape from '../../../helpers/propz/scatShape';
+
 
 class ScatCard extends React.Component {
   static propTypes = {
     scat: scatShape.scatShape,
+    removeScat: PropTypes.func.isRequired,
   }
 
   render() {
-    const { scat } = this.props;
+    const { scat, removeScat } = this.props;
     const singleLink = `/scats/${scat.id}`;
     const editLink = `/edit/${scat.id}`;
+
+
     return (
       <div className="ScatCard col-3">
         <div className="card">
@@ -21,6 +25,7 @@ class ScatCard extends React.Component {
             <p className="card-text">{scat.notes}</p>
             <Link className="btn btn-info m-1" to={singleLink}> View </Link>
             <Link className="btn btn-warning m-1" to={editLink}> Edit </Link>
+            <button className="btn btn-danger m-1" onClick={() => removeScat(scat.id)}> Delete </button>
           </div>
         </div>
       </div>
